e2d5166d60805b6466ef074a77b70a5646caf3c9
Kesava Raj <kesavarajtry@gmail.com>
2024-12-15 21:39:25 +0530

    input and output and service changes done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { ViewChildComponent } from './view-child/view-child.component';
 import { HomeComponent } from './home/home.component';
 import { InputComponent } from './input-decorator/input.component';
-import { OutputComponent } from './output-decorator/output/output.component';
+import { OutputComponent } from './output-decorator/output.component';
 import { ServiceComponent } from './service/service.component';
 
 export const routes: Routes = [
